af389fa3177aa9776a21d6814e49f1b702d07fe9
IRS1318 DriverStation20A <issaquahroboticssociety@outlook.com>
2020-01-16 20:15:55 -0800

    Fix spacing in ColorMechanism

--- core_robot/src/main/java/frc/robot/mechanisms/ColorMechanism.java
+++ core_robot/src/main/java/frc/robot/mechanisms/ColorMechanism.java
@@ -5,7 +5,6 @@ import frc.robot.common.*;
 import frc.robot.common.robotprovider.*;
 import frc.robot.driver.common.Driver;
 import frc.robot.common.robotprovider.IDashboardLogger;
-import frc.robot.vision.common.VisionProcessingState;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -16,21 +15,23 @@ import frc.robot.driver.common.*;
 @Singleton
 public class ColorMechanism implements IMechanism
 {
+    private static final String logName = "color";
+
     private final IDashboardLogger logger;
     private final IColorSensorV3 sensor;
+
     private Driver driver;
-    private static final String logName = "color";
 
     @Inject
     public ColorMechanism(IDashboardLogger logger, IRobotProvider provider)
     {
-        
         this.logger = logger;
         this.sensor = provider.getColorSensor();
     }
 
     @Override
-    public void readSensors() {
+    public void readSensors()
+    {
         RawColorRGBIR rawColor = sensor.getRawColor();
         logger.logNumber(ColorMechanism.logName,"blue",rawColor.getBlue());
         logger.logNumber(ColorMechanism.logName,"red",rawColor.getRed());
@@ -42,23 +43,18 @@ public class ColorMechanism implements IMechanism
     }
 
     @Override
-    public void update() {
+    public void update()
+    {
     }
 
     @Override
-    public void stop() {
-        // TODO Auto-generated method stub
-
+    public void stop()
+    {
     }
 
     @Override
-    public void setDriver(Driver driver) {
+    public void setDriver(Driver driver)
+    {
         this.driver = driver;
-
     }
-    
-    
-
-
-
 }
