7facb476893af7e64cc46a309cd76735005cbf44
Sravani Nanduri <sravani.nanduri.sn@gmail.com>
2020-01-17 18:54:35 -0800

    Adding ColorMatch (getColorMatch) to ColorMechanism

--- core_robot/src/main/java/frc/robot/mechanisms/ColorMechanism.java
+++ core_robot/src/main/java/frc/robot/mechanisms/ColorMechanism.java
@@ -5,6 +5,7 @@ import frc.robot.common.*;
 import frc.robot.common.robotprovider.*;
 import frc.robot.driver.common.Driver;
 import frc.robot.common.robotprovider.IDashboardLogger;
+import frc.robot.common.robotprovider.IColorMatch;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -19,14 +20,16 @@ public class ColorMechanism implements IMechanism
 
     private final IDashboardLogger logger;
     private final IColorSensorV3 sensor;
+    private final IColorMatch color;
 
     private Driver driver;
 
     @Inject
-    public ColorMechanism(IDashboardLogger logger, IRobotProvider provider)
+    public ColorMechanism(IDashboardLogger logger, IRobotProvider provider, IColorMatch color)
     {
         this.logger = logger;
         this.sensor = provider.getColorSensor();
+        this.color = provider.getColorMatch();
     }
 
     @Override
@@ -38,8 +41,17 @@ public class ColorMechanism implements IMechanism
         logger.logNumber(ColorMechanism.logName,"green",rawColor.getGreen());
         logger.logNumber(ColorMechanism.logName,"IR", rawColor.getIR());
 
+        //percentages of color
+        double red = (double)rawColor.getRed()/((double) rawColor.getRed() + (double)rawColor.getGreen() + (double)rawColor.getBlue());
+        double green = (double)rawColor.getGreen()/((double) rawColor.getRed() + (double)rawColor.getGreen() + (double)rawColor.getBlue());
+        double blue = (double)rawColor.getBlue()/((double) rawColor.getRed() + (double)rawColor.getGreen() + (double)rawColor.getBlue());
+
+        color.matchClosestColor(red, green, blue);
+
         int proximity = sensor.getProximity();
         logger.logInteger(ColorMechanism.logName, "proximity", proximity);
+
+
     }
 
     @Override
