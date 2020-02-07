64a8c621cd2051238339b7dceabb1aeb684ed7ce
vanshikaaa <vanshikabalaji@outlook.com>
2020-02-06 20:33:37 -0800

    Made turret align and turret vision center tasks

---
+++ core_robot/src/main/java/frc/robot/driver/controltasks/TurretAlignTask.java
@@ -0,0 +1,43 @@
+package frc.robot.driver.controltasks;
+
+import frc.robot.driver.*;
+import frc.robot.driver.common.IControlTask;
+import frc.robot.mechanisms.*;
+
+
+public class TurretAlignTask extends ControlTaskBase implements IControlTask
+{
+    public PowerCellMechanism powerCell;
+
+    public TurretAlignTask(){
+    }
+
+    @Override
+    public void begin() {
+        this.powerCell = this.getInjector().getInstance(PowerCellMechanism.class);
+    }
+
+    @Override
+    public void update() {
+        this.setAnalogOperationState(AnalogOperation.PowerCellTurretPosition, 0.0);
+
+    }
+
+    @Override
+    public void end() {
+        
+
+    }
+
+    @Override
+	public boolean hasCompleted() {
+        if(powerCell.getTurretPosition() == 0.0){
+            return true;
+        }
+        else{
+            return false;
+        }
+		
+    }
+    
+}
