3b17f77131cda87bab80c296d67fa8bc2a7c4f92
Mike Violette <mviol42@gmail.com>
2020-02-08 15:15:10 -0800

    Updated tracer shot to work with new pc mech

--- core_robot/src/main/java/frc/robot/driver/controltasks/TracerShotTask.java
+++ core_robot/src/main/java/frc/robot/driver/controltasks/TracerShotTask.java
@@ -1,8 +1,6 @@
 package frc.robot.driver.controltasks;
 
-import frc.robot.TuningConstants;
 import frc.robot.common.robotprovider.ITimer;
-import frc.robot.driver.AnalogOperation;
 import frc.robot.driver.DigitalOperation;
 import frc.robot.driver.common.IControlTask;
 import frc.robot.mechanisms.PowerCellMechanism;
@@ -10,53 +8,53 @@ import frc.robot.mechanisms.PowerCellMechanism;
 public class TracerShotTask extends ControlTaskBase implements IControlTask
 {
     private PowerCellMechanism powerCellMechanism;
-    private int currentSlotIndex;
     private ITimer timer;
     private Double kickTime;
 
-    TracerShotTask() {}
+    public TracerShotTask()
+    {
+    }
 
     @Override
-    public void begin() {
+    public void begin()
+    {
         this.powerCellMechanism = this.getInjector().getInstance(PowerCellMechanism.class);  
-        this.currentSlotIndex = powerCellMechanism.getCurrentCarouselIndex();
         this.timer = this.getInjector().getInstance(ITimer.class);
     }
 
     @Override
-    public void update() {
-        boolean toTurn = !this.powerCellMechanism.hasPowerCell(this.powerCellMechanism.getCurrentCarouselIndex());
-
-        if (toTurn && this.powerCellMechanism.getCurrentCarouselIndex() == currentSlotIndex) // if no power cell to kick, rotate
+    public void update()
+    {
+        if (this.kickTime != null || this.powerCellMechanism.hasPowerCell(this.powerCellMechanism.getCurrentCarouselIndex())) 
         {
-            this.setAnalogOperationState(AnalogOperation.PowerCellGenevaPower, TuningConstants.POWERCELL_GENEVA_MECHANISM_MOTOR_POWER);
-        }
-        else if (toTurn && this.powerCellMechanism.getCurrentCarouselIndex() != currentSlotIndex) // if no power cell to kick, but we've rotated one slot, keep rotating
-        {
-            this.currentSlotIndex = this.powerCellMechanism.getCurrentCarouselIndex();
-            this.setAnalogOperationState(AnalogOperation.PowerCellGenevaPower, TuningConstants.POWERCELL_GENEVA_MECHANISM_MOTOR_POWER);
-        }
-        else // if power cell to kick, stop motor and kick piston
-        {
-            this.setAnalogOperationState(AnalogOperation.PowerCellGenevaPower, TuningConstants.STHOPE_BLEASE);
             this.setDigitalOperationState(DigitalOperation.PowerCellKick, true);
-            if(kickTime == null){
+            if (this.kickTime == null)
+            {
                 this.kickTime = this.timer.get();
             }
+            
+        }
+        else
+        {
+            this.setDigitalOperationState(DigitalOperation.PowerCellMoveOneSlot, true);
         }
     }
 
     @Override
-    public void end() {
-        this.setAnalogOperationState(AnalogOperation.PowerCellGenevaPower, TuningConstants.STHOPE_BLEASE);
+    public void end()
+    {
+        this.setDigitalOperationState(DigitalOperation.PowerCellMoveOneSlot, false);
         this.setDigitalOperationState(DigitalOperation.PowerCellKick, false);
     }
 
     @Override
-    public boolean hasCompleted() {
-        if(kickTime != null && this.timer.get() - kickTime >= .5){
+    public boolean hasCompleted()
+    {
+        if (this.kickTime != null && this.timer.get() - this.kickTime >= .5)
+        {
             return true; 
         }
+        
         return false;
     }
 }
