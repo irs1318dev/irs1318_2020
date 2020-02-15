3e5f12761e7bb6558a980a8e384a099a759dff72
DriverStation20B <issaquahroboticssociety@outlook.com>
2020-02-14 19:51:38 -0800

    New preliminary measurement for camera pitch, z-offset

--- core_robot/src/main/java/frc/robot/HardwareConstants.java
+++ core_robot/src/main/java/frc/robot/HardwareConstants.java
@@ -11,9 +11,9 @@ public class HardwareConstants
     //================================================= Vision ======================================================
 
     // Vision Alignment 
-    public static final double CAMERA_PITCH = 1.0; // in degrees
-    public static final double CAMERA_X_OFFSET = 1.0; // in inches
-    public static final double CAMERA_Z_OFFSET = 22.0; // in inches
+    public static final double CAMERA_PITCH = 22.5; // in degrees
+    public static final double CAMERA_X_OFFSET = 0.0; // in inches
+    public static final double CAMERA_Z_OFFSET = 21.0; // in inches
     public static final double VISIONTARGET_Z_OFFSET = 90.25; // in inches
     public static final double CAMERA_TO_TARGET_Z_OFFSET = HardwareConstants.VISIONTARGET_Z_OFFSET - HardwareConstants.CAMERA_Z_OFFSET;
     public static final double CAMERA_YAW = 0.0; // in degrees
